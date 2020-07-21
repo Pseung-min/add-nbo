1b8cacd97913bce485bf6cb011d85dbf708fced6
Pseung-min <psm345601@naver.com>
2020-07-22 01:57:49 +0900

    restore main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+uint32_t get_nbo(FILE* fp);
+
 int main(int argc, char *argv[])
 {
     if (argc != 3)
@@ -15,25 +17,36 @@ int main(int argc, char *argv[])
     if ((fp1 = fopen(argv[1], "rb")) == NULL) return -1;
     if ((fp2 = fopen(argv[2], "rb")) == NULL) return -1;
 
+    uint32_t nbo1, nbo2;
+    nbo1 = get_nbo(fp1);
+    nbo2 = get_nbo(fp2);
+
+    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", nbo1, nbo1, nbo2, nbo2, nbo1+nbo2, nbo1+nbo2);
+    /*
     uint32_t hn1, hn2;
     fread(&hn1, sizeof(uint32_t), 1, fp1);
     fread(&hn2, sizeof(uint32_t), 1, fp2);
-
-//    printf("0x%x\n0x%x\n", hn1, hn2);
 
     uint32_t nn1, nn2, s;
     nn1 = htonl(hn1);
     nn2 = htonl(hn2);
     s = nn1 + nn2;
 
-//    printf("0x%x\n0x%x\n", nn1, nn2);
-//    printf("%d\n%d\n", nn1, nn2);
-
     printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", nn1, nn1, nn2, nn2, s, s);
 //    1000(0x3e8) + 500(0x1f4) = 1500(0x5dc)
-
+*/
     fclose(fp2);
     fclose(fp1);
 
     return 0;
+}
+
+uint32_t get_nbo(FILE* fp)
+{
+    uint32_t nbo, hbo;
+
+    fread(&hbo, 1, sizeof(uint32_t), fp);
+    nbo = htonl(hbo);
+
+    return nbo;
 }
